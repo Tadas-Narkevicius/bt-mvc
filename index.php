d7a83f86d94294ff1f99e874d5142a8424bbcd91
Tadas Narkevicius <tadass411@gmail.com>
2018-10-31 15:53:08 +0200

    second commit

--- index.php
+++ index.php
@@ -1,55 +1,68 @@
 <?php
 
+$filename = 'D:\www\bt-mvc\core\config.inc.php';
+
+if (file_exists($filename)) {     
+    include_once $filename; 
+} else {     
+    die('Structure mismatch: config.inc.php'); 
+} 
+ 
 $indexName = basename(__FILE__); 
+
 $baseDir   = strtr(__DIR__, '/\\', DIRECTORY_SEPARATOR); 
 $baseUrl   = substr($_SERVER['SCRIPT_NAME'], 0, strpos($_SERVER['SCRIPT_NAME'], $indexName)); 
 $pathInfo  = substr($_SERVER['REQUEST_URI'], strlen($baseUrl)); 
-
+ 
+//var_dump($pathInfo);
+require_once CORE_COMPONENTS_DIR . 'Response.php'; 
+ 
 if (!$pathInfo) {     
     $pathInfo = '/'; 
 } 
-
-if (!isset($_SERVER['HTTP_REWRITE_ON'])) {
-    $baseUrl = $baseUrl . $indexName . $pathInfo;  
+ 
+if (!isset($_SERVER['HTTP_REWRITE_ON'])) {     
+    $baseUrl = $baseUrl . $indexName . $pathInfo; 
 } 
-
+ 
 $controllerPrefix = 'Default'; 
 $actionPrefix     = 'default'; 
- 
-$route = explode('/', $pathInfo); 
-$route = array_filter(explode('/', $pathInfo)); 
- 
-$controller       = $controllerPrefix . 'Controller.php'; 
+$route            = explode('/', $pathInfo); 
+$route            = array_filter(explode('/', $pathInfo)); 
+$controller       = CONTROLLER_DIR . $controllerPrefix . 'Controller.php'; 
 $action           = $actionPrefix . 'Action'; 
 $model            = '';
 
-if (!empty($route)) {
-    if ($route[0] == $indexName) {
+if (!empty($route)) {     
+    if ($route[0] == $indexName) {        
         unset($route[0]);         
-    $route = array_values($route);     
-    }     
-    if ((isset($route[0])) && ($route[0])) {
-        $controller = str_replace($controllerPrefix, 
-        ucfirst(strtolower($route[0])), $controller); 
+        $route = array_values($route);     
     }
-    if ((isset($route[1])) && ($route[1])) {
+    if ((isset($route[0])) && ($route[0])) {         
+        $controller = str_replace($controllerPrefix, 
+        ucfirst(strtolower($route[0])), $controller);     
+    }     
+    if ((isset($route[1])) && ($route[1])) {         
         $action = str_replace($actionPrefix, strtolower($route[1]), $action);     
     }     
-    if ((isset($route[2])) && ($route[2])) {
-        $model = $route[2];    
+    if ((isset($route[2])) && ($route[2])) {         
+        $model = $route[2];     
     } 
-} 
+}
 
-if (file_exists($controller)) {
+if (file_exists($controller)) {     
     include_once $controller;     
-    if (function_exists($action)) {
+    if (function_exists($action)) {         
         $result = $action($model);         
-        echo $result;     
+        echo $result;         
+        exit();     
     } else {         
-        http_response_code('404');     
+        http_response_code('404');         
+        exit();     
     } 
-} else {    
-    http_response_code('400'); 
+} else {     
+    http_response_code('400');     
+    exit(); 
 }
 
 
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+
+function defaultAction($modelId = null) {     
+    return renderResponse(null,          
+    array('msgFrom' => 'Tadas',              
+        'msgTo' => 'Eglė',             
+        'msgSubj' => 'Dėl rytojaus',             
+        'msgBody' => 'Rytoj PHP kursas kaip visada – 17:30',         
+    ), 'xml'); 
+} 
